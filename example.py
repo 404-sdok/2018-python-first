fdf1bb3bdf4b3ad4f2aa468e0b1e91263fcb9bb1
hoparkgo9ma <me@ho9.me>
2018-08-18 22:31:44 +0900

    커-밋

---
+++ example.py
@@ -0,0 +1,13 @@
+# === 이러면 실격
+print('10224 박종현')
+
+# === 예시 2
+name = '박종현'
+class_data = '10224'
+print(class_data + name)
+
+# === 예시 2
+name = '박종현'
+class_data = '10224'
+content = class_data + name
+print(content)
